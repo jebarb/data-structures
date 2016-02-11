5198eaa5b4a27bf647022f493c3a88f5480770cc
James Barbour <jebarbou@cs.unc.edu>
2016-02-11 00:47:38 -0500

    BST mostly finished, remove being fucky

--- a3/assn3.java
+++ a3/assn3.java
@@ -1,17 +1,23 @@
-class Assn3Main {
-    class Assn3Main {
-        public static void main (String[] args) {
-            // in here code for doing the
-            // interactive builder/tester mode
-            // and the command line arg auto-execution mode
-        }
-    }
-}
+/*
+   class Assn3Main {
+   class Assn3Main {
+   public static void main (String[] args) {
+// in here code for doing the
+// interactive builder/tester mode
+// and the command line arg auto-execution mode
+   }
+   }
+   }
+   */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 class BST {
 
-    private node root;
-    private int size, height;
+    private Node root;
+    private int size;
 
     private class Node {
         private String data;
@@ -24,62 +30,114 @@ class BST {
         }
 
         private Node getNode(String s) {
-            if (data == null) return null;
             if (data.equals(s)) return this;
             if (data.compareTo(s) > 0) {
                 if (left == null) return null;
                 else return left.getNode(s);
-            } else {
+            } else if (data.compareTo(s) < 0) {
                 if (right == null) return null;
                 else return right.getNode(s);
+            } else return null;
+        }
+
+        private boolean insert(String s) {
+            if (data == null) data = s;
+            else if (data.compareTo(s) > 0) {
+                if (left == null) {
+                    left = new Node(s, null, null);
+                } else return left.insert(s);
+            } else if (data.compareTo(s) < 0) {
+                if (right == null) {
+                    right = new Node(s, null, null);
+                } else return right.insert(s);
+            } else return false;
+            return true;
+        }
+
+        private boolean remove(String s) {
+            if (data.equals(s)) {
+                if (left != null) {
+                    data = left.findMax().data;
+                    if (data.equals(left.data)) left = null;
+                    else left.remove(data);
+                    return true;
+                } else if (right != null) {
+                    data = right.findMin().data;
+                    if (data.equals(right.data)) right = null;
+                    else right.remove(data);
+                    return true;
+                } else {
+                    data = null;
+                    return false;
+                }
+            } else if (data.compareTo(s) > 0) {
+                if (left == null) return false;
+                else if (!left.remove(s)) left = null;
+                return true;
+            } else if (data.compareTo(s) < 0) {
+                if (right == null) return false;
+                else if (!right.remove(s)) right = null;
+                return true;
             }
+            return false;
         }
 
         private Node findMin() {
+            if (left != null) return left.findMin();
+            else return this;
         }
 
         private Node findMax() {
+            if (right != null) return right.findMax();
+            else return this;
         }
 
-        private int getHeight() {
+        private int getHeight(int i) {
+            int l = 0;
+            int r = 0;
+            if (left != null) l = left.getHeight(i+1);
+            if (right != null) r += right.getHeight(i+1);
+            return (l > r) ? l : r;
         }
 
-        private int getSize() {
+        private int getSize(int i) {
+            if (left != null) i = left.getSize(i+1);
+            if (right != null) i = right.getSize(i+1);
+            return i;
         }
 
     }
 
     //new       :          -->  BST
-    public BST(Node root, int size, int height) {
-        this.root = root;
-        this.size = size;
-        this.height = height;
-
+    public BST() {
+        this(null);
     }
 
-    public BST() {
-        this(new Node(null, null, null), 0, 0);
+    public BST(String s) {
+        this.root = new Node(s, null, null);
+        this.size = 1;
+    }
+
+    public BST(Node root, int size) {
+        this.root = root;
+        this.size = size;
     }
 
 
     //insert    :  String  -->      (or maybe a boolean showing success)
     public boolean insert(String s) {
-        if (root.insert(s) != null) {
-            size++;
+        if (root.insert(s)) {
+            size--;
             return true;
-        } else {
-            return false;
-        }
+        } else return false;
     }
 
     //remove    :  String  -->      (or maybe a boolean showing success)
     public boolean remove(String s) {
-        if (root.remove(s) != null) {
+        if (root.remove(s)) {
             size--;
             return true;
-        } else {
-            return false;
-        }
+        } else return false;
     }
 
     //findMin   :          -->  String
@@ -99,7 +157,8 @@ class BST {
 
     //get       :  String  -->  BST  (a node actually)
     public BST get(String s) {
-        return new BST(root.getNode(s), root.getSize(), root.getHeight());
+        Node n = root.getNode(s);
+        return new BST(n, n.getSize(1));
     }
 
     //val       :          -->  String  (returns the key stored in the root)
@@ -119,10 +178,21 @@ class BST {
 
     //height    :          -->  int+ (an integer zero or larger)
     public int height() {
-        return height;
+        return root.getHeight(0);
+    }
+
+    public BST getLeft() {
+        if (root.left != null) {
+            return new BST(root.left, root.left.getSize(1));
+        } else return null;
+    }
+
+    public BST getRight() {
+        if (root.right != null) {
+            return new BST(root.right, root.right.getSize(1));
+        } else return null;
     }
 }
-
 class MyRandom {
 
     private static Random rn = new Random();
@@ -141,7 +211,7 @@ class MyRandom {
         byte b[] = new byte[n];
         for (int i = 0; i < n; i++)
             b[i] = (byte)rand('a', 'z');
-        return new String(b, 0);
+        return new String(b);
     }
 
     public static String nextString() {
@@ -150,36 +220,39 @@ class MyRandom {
 
 }
 
-import java.util.ArrayList;
-import java.util.List;
 
-public class TreePrinter {
+class TreePrinter {
 
     public static void main(String[] args) {
-        // test 1
-        BST root = new BST("c");
-        BST left = new BST("b");
-        left.left = new BST("a");
-        BST right = new BST("e");
-        right.left = new BST("d");
-        right.right = new BST("f");
-        root.left = left;
-        root.right = right;
 
-        print(root);
+        BST root = new BST("c");
+        root.insert("b");
+        root.insert("a");
+        root.insert("e");
+        root.insert("d");
+        root.insert("f");
+
+        //print(root);
+
+        //root.remove("c");
+
+        //print(root);
 
         // test 2
-        root = new BST("h");
-        for (String val : new String[] { "b", "r", "f", "a", "d", "q", "g" })
-            root.ins(val);
-        print(root);
+        root = new BST();
+        for (String val : new String[] { "h", "b", "r", "f", "a", "d", "q", "g" }) {
+            root.insert(val);
+            print(root);
+        }
 
+        for (String val : new String[] { "h", "g", "f", "d", "b", "a", "q", "r" }) {
+            print(root);
+            root.remove(val);
+        }
         // test 3
-        root = new BST("much longer Strings also work");
-        for (String val : new String[] { "longer than one character", 
-            "some other test string", "another test string" })
-            root.ins(val);
-        print(root);
+        root = new BST();
+        for (int i = 0; i < 15; i++) root.insert(MyRandom.nextString(1,2));
+        //print(root);
     }
 
     public static void print(BST root) {
@@ -205,17 +278,17 @@ public class TreePrinter {
                     next.add(null);
                     next.add(null);
                 } else {
-                    String aa = n.root;
+                    String aa = n.val();
                     line.add(aa);
                     if (aa.length() > widest)
                         widest = aa.length();
 
-                    next.add(n.left);
-                    next.add(n.right);
+                    next.add(n.getLeft());
+                    next.add(n.getRight());
 
-                    if (n.left != null)
+                    if (n.getLeft() != null)
                         nn++;
-                    if (n.right != null)
+                    if (n.getRight() != null)
                         nn++;
                 }
             }
