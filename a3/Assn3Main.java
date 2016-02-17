b8b4377f1249a5da58c6246aad6e162b62192513
James Barbour <jebarbou@cs.unc.edu>
2016-02-17 08:41:50 -0500

    final a3 submission

--- a3/Assn3Main.java
+++ a3/Assn3Main.java
@@ -2,22 +2,25 @@ import java.util.Random;
 import java.util.Scanner;
 import java.io.File;
 import java.util.InputMismatchException;
+import java.io.FileNotFoundException;
 
 class Assn3Main {
 
     private static Scanner s;
-    // = new Scanner(System.in);
     private static BST bst = new BST();
 
     private static void printHelp() {
         System.out.println("Usage: [OPTION]... [OPTION] [ARGUMENT]...");
         System.out.println("Options:");
         System.out.println(
+                "  help      : print this message\n" +
                 "  new       : make a new empty tree\n" +
-                "  i [STRING]: insert a string (get the value from the user)\n" +
-                "  I [FILE]  : fill the tree with strings from file, report true if no duplicates\n" +
+                "  i [STRING]: insert a string\n" +
+                "  I [FILE]  : fill the tree with strings from file,\n" + 
+                "                  report true if no duplicates\n" +
                 "  r [STRING]: remove a node containing a string\n" +
-                "  R [STRING]: remove a nodes containing strings from file, report true if all strings are removed with no duplicates\n" +
+                "  R [STRING]: remove a nodes containing strings from file,\n" + 
+                "                  report true if all strings are removed with no duplicates\n" +
                 "  c [STRING]: contains a string, report a boolean indicating success\n" +
                 "  C [FILE]  : contains all strings in a file, report boolean indicating success\n" +
                 "  g [STRING]: get a node that has a string as value and assign to BST\n" +
@@ -29,42 +32,11 @@ class Assn3Main {
                 "  h         : height\n" +
                 "  q         : quit the tester loop\n" +
                 "  p         : print the tree for inspection\n" +
-                "  f [INT]   : fill the tree with some amount of random string data\n" +
-                "  help      : print this message\n");
-    }
-
-    private static void print(BST bst, int height, String s, String lines) {
-        int i;
-        if (bst == null) {
-            System.out.println("null");
-            return;
-        }
-        for (i = 0; i < height; i++) {
-            if (lines.charAt(i) == '1') System.out.printf(" |");
-            else System.out.printf("  ");
-        }
-        if (height != 0) {
-            if (lines.charAt(i) == '1')
-                System.out.print(" ├");
-            else System.out.print(" └");
-            System.out.printf(height + " " + s + ": " + bst.val() + "\n");
-        } else {
-            System.out.printf(s + ": " + bst.val() + "\n");
-        }
-        BST left = bst.getLeft();
-        BST right = bst.getRight();
-        height++;
-        String temp = new String(lines);
-        if (left != null) {
-            if (right != null) print(left, height, "left", temp.concat("1"));
-            else print(left, height, "left", temp.concat("0"));
-        }
-        if (right != null) print(right, height, "right", lines.concat("0"));
+                "  f [INT]   : fill the tree with some amount of random string data\n");
     }
 
     private static void process() {
         String str = s.next("\\S*");
-        //System.out.println(s.next());
         switch (str) {
             case "new":
                 bst = new BST();
@@ -80,8 +52,12 @@ class Assn3Main {
                 System.out.println(bst.contains(s.next()) + "\n");
                 break;
             case "g": 
-                //print(bst.get(s.next()), 0, "root", "0");
-                bst = bst.get(s.next());
+                //Uncomment first line to print subtree
+                //Uncomment second line to print string in node
+                //Uncomment third line to reassign bst to subtree
+                bst.get(s.next()).print();
+                //System.out.println(bst.get(s.next()).val());
+                //bst = bst.get(s.next());
                 System.out.println();
                 break;
             case "x": 
@@ -104,8 +80,9 @@ class Assn3Main {
                 break;
             case "q":
                 System.exit(0);
+                break;
             case "p":
-                print(bst, 0, "root", "0");
+                bst.print();
                 System.out.println();
                 break;
             case "f":
@@ -113,22 +90,20 @@ class Assn3Main {
                 try {
                     num = s.nextInt();
                 } catch (InputMismatchException ex) {
-                    System.out.println("Invalid argument");
+                    System.out.println("Invalid argument\n");
                 }
-                for (int i = 0; i < num; i++) {
+                for (int i = 0; i < num; i++)
                     bst.insert(MyRandom.nextString(5,15));
-                }
                 break;
             case "C":
                 try {
                     File file_in = new File(s.next());
                     Scanner file = new Scanner(file_in);
                     boolean res = true;
-                    while (file.hasNext()) {
+                    while (file.hasNext())
                         if (!bst.contains(file.next())) res = false;
-                    }
                     System.out.println(res + "\n");
-                } catch (Exception ex) {
+                } catch (FileNotFoundException ex) {
                     System.out.println("Invalid argument\n");
                 }
                 break;
@@ -137,11 +112,10 @@ class Assn3Main {
                     File file_in = new File(s.next());
                     Scanner file = new Scanner(file_in);
                     boolean res = true;
-                    while (file.hasNext()) {
+                    while (file.hasNext())
                         if (!bst.insert(file.next())) res = false;
-                    }
                     System.out.println(res + "\n");
-                } catch (Exception ex) {
+                } catch (FileNotFoundException ex) {
                     System.out.println("Invalid argument\n");
                 }
                 break;
@@ -150,12 +124,11 @@ class Assn3Main {
                     File file_in = new File(s.next());
                     Scanner file = new Scanner(file_in);
                     boolean res = true;
-                    while (file.hasNext()) {
+                    while (file.hasNext())
                         if (!bst.remove(file.next())) res = false;
-                    }
                     System.out.println(res + "\n");
-                } catch (Exception ex) {
-                    System.out.println("Invalid argument\n");
+                } catch (FileNotFoundException ex) {
+                    System.err.println("Invalid argument\n");
                 }
                 break;
             case "help":
@@ -168,19 +141,16 @@ class Assn3Main {
         if (args.length == 0) {
             s = new Scanner(System.in);
             System.out.println("Type 'help' for usage");
-            while (true) {
+            while (true)
                 process();
-            }
         } else {
             String in = "";
-            for (int i = 0; i < args.length; i++) {
+            for (int i = 0; i < args.length; i++)
                 in = in.concat(args[i] + " ");
-            }
             System.out.println(in);
             s = new Scanner(in);
-            while (s.hasNext()) {
+            while (s.hasNext())
                 process();
-            }
         }
 
     }
@@ -216,13 +186,11 @@ class BST {
         private boolean insert(String s) {
             if (data == null) data = s;
             else if (data.compareTo(s) > 0) {
-                if (left == null) {
-                    left = new Node(s, null, null);
-                } else return left.insert(s);
+                if (left == null) left = new Node(s, null, null);
+                else return left.insert(s);
             } else if (data.compareTo(s) < 0) {
-                if (right == null) {
-                    right = new Node(s, null, null);
-                } else return right.insert(s);
+                if (right == null) right = new Node(s, null, null);
+                else return right.insert(s);
             } else return false;
             return true;
         }
@@ -238,9 +206,7 @@ class BST {
                     data = right.findMin().data;
                     right.remove(data);
                     if (right.data == null) right = null;
-                } else {
-                    data = null;
-                }
+                } else data = null;
                 return true;
             } else if (data.compareTo(s) > 0) {
                 if (left == null) return false;
@@ -274,15 +240,39 @@ class BST {
             return (l > r) ? l : r;
         }
 
-        private int getSize(int i) {
-            if (left != null) i = left.getSize(i+1);
-            if (right != null) i = right.getSize(i+1);
+        private int getSize() {
+            int i = 1;
+            if (data == null) return 0;
+            if (left != null) i += left.getSize();
+            if (right != null) i += right.getSize();
             return i;
         }
 
+        private void print(int depth, String side, String lines) {
+            int i;
+            if (data == null) {
+                System.out.println("null");
+                return;
+            }
+            for (i = 0; i < depth; i++) {
+                if (lines.charAt(i) == '1') System.out.printf(" |");
+                else System.out.printf("  ");
+            }
+            if (depth != 0) {
+                if (lines.charAt(i) == '1') System.out.print(" ├");
+                else System.out.print(" └");
+                System.out.printf(depth + " " + side + ": " + data + "\n");
+            } else System.out.printf(side + ": " + data + "\n");
+            depth++;
+            String temp = new String(lines);
+            if (left != null) {
+                if (right != null) left.print(depth, "left", temp.concat("1"));
+                else left.print(depth, "left", temp.concat("0"));
+            }
+            if (right != null) right.print(depth, "right", lines.concat("0"));
+        }
     }
 
-    //new       :          -->  BST
     public BST() {
         this.root = new Node(null, null, null);
         this.size = 0;
@@ -298,8 +288,6 @@ class BST {
         this.size = size;
     }
 
-
-    //insert    :  String  -->      (or maybe a boolean showing success)
     public boolean insert(String s) {
         if (root.insert(s)) {
             size++;
@@ -307,7 +295,6 @@ class BST {
         } else return false;
     }
 
-    //remove    :  String  -->      (or maybe a boolean showing success)
     public boolean remove(String s) {
         if (root.remove(s)) {
             size--;
@@ -315,59 +302,46 @@ class BST {
         } else return false;
     }
 
-    //findMin   :          -->  String
     public String findMin() {
         return root.findMin().data;
     }
 
-    //findMax   :          -->  String
     public String findMax() {
         return root.findMax().data;
     }
 
-    //contains  :  String  -->  boolean
     public boolean contains(String s) {
         return root.getNode(s) != null;
     }
 
-    //get       :  String  -->  BST  (a node actually)
     public BST get(String s) {
         Node n = root.getNode(s);
-        return (n == null) ? null : new BST(n, n.getSize(1));
+        return (n == null) ? null : new BST(n, n.getSize());
     }
 
-    //val       :          -->  String  (returns the key stored in the root)
     public String val() {
         return root.data;
     }
 
-    //empty     :          -->  boolean
     public boolean empty() {
         return size == 0;
     }
 
-    //size      :          -->  int+ (an integer zero or larger)
     public int size() {
         return size;
     }
 
-    //height    :          -->  int+ (an integer zero or larger)
     public int height() {
         return root.getHeight();
     }
 
-    public BST getLeft() {
-        if (root.left != null) {
-            return new BST(root.left, root.left.getSize(1));
-        } else return null;
+    public void print() {
+        root.print(0, "root", "0");
     }
 
-    public BST getRight() {
-        if (root.right != null) {
-            return new BST(root.right, root.right.getSize(1));
-        } else return null;
-    }
+
 }
+
 class MyRandom {
 
     private static Random rn = new Random();
