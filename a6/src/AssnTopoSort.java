2f3a78841ed62a0ccec0484b4ccf2d0e0ab8ae1b
James Barbour <jebarbou@cs.unc.edu>
2016-04-14 00:26:11 -0400

    final-ish, maybe clean up and test more

--- a6/src/AssnTopoSort.java
+++ a6/src/AssnTopoSort.java
@@ -3,14 +3,59 @@ import java.util.Arrays;
 public class AssnTopoSort {
 
     public static void main(String[] args) {
+/*
+    public void randomFill(int n) {
+        int m = n;
+        while (m-- > 0)
+            addNode(MyRandom.rand(0, 64 * n), MyRandom.nextString());
+        for (Vertex v : vertices.values()) {
+            int p = 10;
+            for (Vertex e : vertices.values()) {
+                if (p > 0) addEdge(p*MyRandom.rand(1, 64 * n), v.name, e.name, 1, null);
+                p--;
+            }
+        }
+    }
+
+    public void randomRemoveNode(int n) {
+        LinkedList<String> strings = new LinkedList<>();
+        for (Vertex v : vertices.values()) {
+            if (n > 0) strings.add(v.name);
+            n--;
+        }
+        strings.forEach(s -> delNode(s));
+    }
+
+    public void randomRemoveEdge(int n) {
+        LinkedList<String> stringsv = new LinkedList<>();
+        LinkedList<String> stringse = new LinkedList<>();
+        for (Vertex v : vertices.values()) {
+            if (n > 0) {
+                long count = n / numNodes() + 1;
+                for (Edge e : v.out_edges.values()) {
+                    if (count > 0) {
+                        stringsv.push(v.name);
+                        stringse.push(e.name);
+                    }
+                    count--;
+                }
+                n--;
+            }
+        }
+        while (stringsv.size() != 0)
+            delEdge(stringsv.pop(), stringse.pop());
+    }
+    */
         DiGraph g = new DiGraph();
-        /*
+
+
         String[] vertices = {"Raleigh", "Durham", "Chapel Hill", "Graham", "Carrboro", "Cary", "Pittsboro", "Sanford", "Los Angeles", "Hillsboro"};
         int i = 0;
         for (String s: vertices) {
             g.addNode(i, s);
             i++;
         }
+
         g.addEdge(++i, "Raleigh", "Durham", 14, ".");
         g.addEdge(++i, "Durham", "Hillsboro", 9, ".");
         g.addEdge(++i, "Chapel Hill", "Graham", 25, ".");
@@ -23,22 +68,26 @@ public class AssnTopoSort {
         g.addEdge(++i, "Pittsboro", "Cary", 19, ".");
         g.addEdge(++i, "Pittsboro", "Sanford", 15, ".");
         g.addEdge(++i, "Sanford", "Los Angeles", 1007, ".");
-        //g.randomFill(10);
-        */
+
+        /*
         g.addNode(1, "a");
         g.addNode(2, "b");
         g.addNode(3, "c");
         g.addEdge(2, "a", "b", 1, null);
         g.addEdge(1, "a", "c", 1, null);
         g.addEdge(3, "c", "b", 1, null);
+        */
+        //g.randomFill(10000);
         g.print();
         String[] str = g.topoSort();
         if (str != null) System.out.println(Arrays.toString(str));
         System.out.println(g.numEdges() + " " + g.numNodes());
 
-        //g.randomRemoveEdge(100);
-        //g.randomRemoveNode(10);
+        //g.randomRemoveEdge(100000);
+        //g.randomRemoveNode(10000);
         //g.print();
+        //str = g.topoSort();
+        //if (str != null) System.out.println(Arrays.toString(str));
         //System.out.println(g.numEdges() + " " + g.numNodes());
     }
 }
--- a6/src/DiGraph.java
+++ a6/src/DiGraph.java
@@ -2,90 +2,44 @@ import java.util.*;
 
 public class DiGraph implements DiGraphInterface {
 
-    public void randomFill(int n) {
-        int m = n;
-        while (m-- > 0)
-            addNode(MyRandom.rand(0, 10 * n), MyRandom.nextString());
-        for (Vertex v : vertices.values()) {
-            int count = MyRandom.rand(1, n);
-            for (Vertex e : vertices.values()) {
-                if (count > 0) addEdge(MyRandom.rand(1, 10 * n), v.name, e.name, MyRandom.rand(0, 5), null);
-                count--;
-            }
-        }
-    }
-
-    public void randomRemoveNode(int n) {
-        LinkedList<String> strings = new LinkedList<>();
-        for (Vertex v : vertices.values()) {
-            if (n > 0) strings.add(v.name);
-            n--;
-        }
-        strings.forEach(s -> delNode(s));
-    }
-
-    public void randomRemoveEdge(int n) {
-        LinkedList<String> stringsv = new LinkedList<>();
-        LinkedList<String> stringse = new LinkedList<>();
-        for (Vertex v : vertices.values()) {
-            if (n > 0) {
-                long count = n / numNodes() + 1;
-                for (Edge e : v.in_edges.values()) {
-                    if (count > 0) {
-                        stringsv.push(v.name);
-                        stringse.push(e.name);
-                    }
-                    count--;
-                }
-                n--;
-            }
-        }
-        while (stringsv.size() != 0)
-            delEdge(stringsv.pop(), stringse.pop());
-    }
-
-    private LinkedHashMap<String, Vertex> vertices;
-    private LinkedHashSet<Long> edge_ids, vertex_ids;
+    private HashMap<String, Vertex> vertices;
+    private HashSet<Long> edge_ids, vertex_ids;
 
     private class Vertex {
-        String name;
         boolean marked;
         private long id;
-        private LinkedHashMap<String, Edge> out_edges, in_edges;
+        private HashMap<String, Edge> out_edges;
+        private HashSet<String> in_edges;
 
-
-        private Vertex(String name, long id) {
-            this.name = name;
+        private Vertex(long id) {
             this.id = id;
-            this.in_edges = new LinkedHashMap<>();
-            this.out_edges = new LinkedHashMap<>();
+            this.out_edges = new HashMap<>();
+            this.in_edges = new HashSet<>();
             this.marked = false;
         }
     }
 
     private class Edge {
-        String name, e_label;
+        String label;
         private long id, weight;
 
-        private Edge(String name, long id, long weight, String e_label) {
-            this.name = name;
-            this.e_label = e_label;
+        private Edge(long id, long weight, String label) {
+            this.label = label;
             this.weight = weight;
             this.id = id;
         }
     }
 
     public DiGraph() {
-        this.vertices = new LinkedHashMap<>();
-        this.vertex_ids = new LinkedHashSet<>();
-        this.edge_ids = new LinkedHashSet<>();
-
+        this.vertices = new HashMap<>();
+        this.vertex_ids = new HashSet<>();
+        this.edge_ids = new HashSet<>();
     }
 
     @Override
     public boolean addNode(long idNum, String label) {
-        if (idNum >= 0 && !vertex_ids.contains(idNum) && label != null &&
-                vertices.putIfAbsent(label, new Vertex(label, idNum)) == null) {
+        if (idNum >= 0 && label != null && !vertex_ids.contains(idNum) &&
+                vertices.putIfAbsent(label, new Vertex(idNum)) == null) {
             vertex_ids.add(idNum);
             return true;
         } else return false;
@@ -96,8 +50,8 @@ public class DiGraph implements DiGraphInterface {
         if (idNum >= 0 && sLabel != null && dLabel != null &&
                 vertices.containsKey(dLabel) && !edge_ids.contains(idNum)) {
             Vertex v = vertices.get(sLabel);
-            if (v != null && v.out_edges.putIfAbsent(dLabel, new Edge(dLabel, idNum, weight, eLabel)) == null) {
-                vertices.get(dLabel).in_edges.put(sLabel, new Edge(sLabel, idNum, weight, eLabel));
+            if (v != null && v.out_edges.putIfAbsent(dLabel, new Edge(idNum, weight, eLabel)) == null) {
+                vertices.get(dLabel).in_edges.add(sLabel);
                 edge_ids.add(idNum);
                 return true;
             } else return false;
@@ -108,19 +62,18 @@ public class DiGraph implements DiGraphInterface {
     public boolean delNode(String label) {
         Vertex vert = vertices.get(label);
         if (vert == null) return false;
-        vert.out_edges.values().forEach(e -> {
-            edge_ids.remove(e.id);
-            vertices.get(e.name).in_edges.remove(label);
-        });
+        vertices.remove(label);
         vertex_ids.remove(vert.id);
-        if (vertices.remove(label) != null) {
-            vert.in_edges.values().forEach(e -> {
-                Vertex v = vertices.get(e.name);
-                edge_ids.remove(v.out_edges.get(label).id);
-                v.out_edges.remove(label);
-            });
-            return true;
-        } else return false;
+        vert.out_edges.entrySet().forEach(e -> {
+            edge_ids.remove(e.getValue().id);
+            vertices.get(e.getKey()).in_edges.remove(label);
+        });
+        vert.in_edges.forEach(e -> {
+            Vertex v = vertices.get(e);
+            edge_ids.remove(v.out_edges.get(label).id);
+            v.out_edges.remove(label);
+        });
+        return true;
     }
 
     @Override
@@ -149,12 +102,13 @@ public class DiGraph implements DiGraphInterface {
 
     @Override
     public void print() {
-        vertices.values().forEach(v -> {
-            System.out.println("(" + v.id + ")" + v.name);
-            v.out_edges.values().forEach(e -> {
-                if (e.e_label != null)
-                    System.out.println("  (" + e.id + ")--" + e.e_label + "," + e.weight + "--> " + e.name);
-                else System.out.println("  (" + e.id + ")--" + e.weight + "--> " + e.name);
+        vertices.entrySet().forEach(v -> {
+            System.out.println("(" + v.getValue().id + ")" + v.getKey());
+            v.getValue().out_edges.entrySet().forEach(e -> {
+                if (e.getValue().label != null)
+                    System.out.println("  (" + e.getValue().id + ")--" + e.getValue().label +
+                            "," + e.getValue().weight + "--> " + e.getKey());
+                else System.out.println("  (" + e.getValue().id + ")--" + e.getValue().weight + "--> " + e.getKey());
             });
         });
     }
@@ -162,29 +116,23 @@ public class DiGraph implements DiGraphInterface {
     @Override
     public String[] topoSort() {
         String[] res = new String[vertices.size()];
-        LinkedHashSet<String> unmarked = new LinkedHashSet<>();
+        HashSet<String> unmarked = new HashSet<>();
         unmarked.addAll(vertices.keySet());
         while (unmarked.size() != 0)
             if (!visit(unmarked.iterator().next(), res, unmarked)) return null;
         return res;
     }
 
-    private boolean visit(String s, String[] res, LinkedHashSet<String> unmarked) {
+    private boolean visit(String s, String[] res, HashSet<String> unmarked) {
         Vertex v = vertices.get(s);
         if (v.marked) return false;
-        else if (!unmarked.contains(s)) return true;
-        else {
-            v.marked = true;
-            for (Edge e : v.out_edges.values()) {
-                Vertex tmp = vertices.get(e.name);
-                if (tmp != null)
-                    if (!visit(tmp.name, res, unmarked)) return false;
-            }
-            unmarked.remove(v.name);
-            v.marked = false;
-            res[unmarked.size()] = s;
-            return true;
-        }
-
+        if (!unmarked.contains(s)) return true;
+        v.marked = true;
+        for (Map.Entry<String, Edge> e : v.out_edges.entrySet())
+            if (!visit(e.getKey(), res, unmarked)) return false;
+        unmarked.remove(s);
+        v.marked = false;
+        res[unmarked.size()] = s;
+        return true;
     }
 }
